fa7bfdd8cf64fbd7596017bc0a9004d51bbfd0d7
jb974 <bertil974@hotmail.fr>
2023-11-07 16:33:54 +0100

    ajoute pagination en js

--- public/asset/js/usedCarFiltre.js
+++ public/asset/js/usedCarFiltre.js
@@ -74,7 +74,7 @@ function addUsedCar( viewPage , data){
     let div = document.createElement('div');
     div.className = "col-md-4";
 
-    let { prix , kilometrage , anneeFabrication , pathImage} = data;
+    let { id , prix , kilometrage , anneeFabrication , pathImage} = data;
 
     div.innerHTML =`
         <div class="card mb-3" style= "max-width: 540px;">
@@ -90,6 +90,9 @@ function addUsedCar( viewPage , data){
                     </div>
                 </div>
             </div>
+             <div class="card-footer d-flex justify-content-center">
+                <a href="/Voiture-Occassion/${id} "  class="btn btn-dark w-75"> Voir </a>
+            </div>
         </div>
     `;
     viewPage.appendChild(div);
@@ -98,23 +101,6 @@ function addUsedCar( viewPage , data){
 const viewPage = document.querySelector('#viewPage');
 const btnValid = document.querySelector('#btnFiltre');
 
-btnValid.addEventListener('click',e=>{
-    e.preventDefault();
-    let attribut = '?minPrix='+minPrix+'&maxPrix='+maxPrix+
-        '&minKm='+minKm+'&maxKm='+maxKm+
-        '&minDate='+minDate+'&maxDate='+maxDate;
-    //console.log('pa',attribut)
-    fetch(btnValid.dataset.link+attribut)
-        .then(reponse => reponse.json())
-        .then(data =>{
-            //console.log(data.dataUsedCar)
-            viewPage.innerHTML ='';
-            data.dataUsedCar.forEach(data =>{
-                supprimePaginator()
-                addUsedCar(viewPage ,JSON.parse(data));
-            })
-        })
-})
 function recupData(value ,element)
 {
     switch (element.dataset.name) {
@@ -149,9 +135,120 @@ function supprimePaginator()
 {
     if(paginator)
     {
-        paginator.remove();
+        paginator.innerHTML = '';
     }
 }
 
+// ============== pagination ===============
+let dataList = [/* Votre liste de données */];
+const itemsPerPage = 9; // Nombre d'éléments à afficher par page
+const paginationContainer = document.getElementById("pagination");
+const dataListContainer = document.getElementById("data-list");
+const divPaginator = document.createElement('div');
+divPaginator.className ="pagination";
+
+let currentPage = 1;
+
+function displayList() {
+    viewPage.innerHTML = "";
+    const start = (currentPage - 1) * itemsPerPage;
+    const end = start + itemsPerPage;
+    const paginatedData = dataList.slice(start, end);
+
+    for (const data of paginatedData) {
+
+        addUsedCar(viewPage, JSON.parse(data));
+    }
+}
+
+function createPaginationButtons() {
+    const totalPages = Math.ceil(dataList.length / itemsPerPage);
+    paginationContainer.innerHTML = "";
+
+    if (totalPages > 1) {
+        const previousButton = document.createElement("button");
+        const span = document.createElement("span");
+        span.className ="page-item"
+        previousButton.className="page-link"
+        previousButton.textContent = "Précédent";
+        previousButton.addEventListener("click", () => {
+            if (currentPage > 1) {
+                currentPage--;
+                activeClass(divPaginator.childNodes[currentPage].firstChild);
+                displayList();
+            }
+        });
+        span.appendChild(previousButton)
+        divPaginator.appendChild(span);
+
+        for (let i = 1; i <= totalPages; i++) {
+            const pageButton = document.createElement("button");
+            const span = document.createElement("span");
+            active = '';
+            dgClass ="";
+            if(currentPage == i ){
+                dgClass = 'bg-danger';
+                active = 'active';
+            }
+            span.className =`page-item ${active}`;
+            pageButton.className=`page-link ${dgClass}`;
+
+            pageButton.textContent = i;
+            pageButton.addEventListener("click", () => {
+                currentPage = i;
+                activeClass(pageButton);
+                displayList();
+            });
+            span.appendChild(pageButton)
+            divPaginator.appendChild(span);
+        }
+
+        const nextButton = document.createElement("button");
+        nextButton.textContent = "Suivant";
+        const span2 = document.createElement("span");
+        span2.className ="page-item"
+        nextButton.className="page-link"
+        nextButton.addEventListener("click", () => {
+            if (currentPage < totalPages) {
+                currentPage++;
+                activeClass(divPaginator.childNodes[currentPage].firstChild);
+                displayList();
+            }
+        });
+        span2.appendChild(nextButton);
+        divPaginator.appendChild(span2);
+        paginationContainer.appendChild(divPaginator);
+    }
+}
+function activeClass(element){
 
 
+    let span = element.parentNode.parentNode.querySelector('span.active');
+    span.classList.remove('active');
+    span.firstElementChild.classList.remove('bg-danger');
+    element.parentNode.classList.add('active');
+    element.classList.add('bg-danger');
+
+}
+
+//displayList();
+
+
+// appelle data filtrer
+btnValid.addEventListener('click',e=>{
+    e.preventDefault();
+    let attribut = '?minPrix='+minPrix+'&maxPrix='+maxPrix+
+        '&minKm='+minKm+'&maxKm='+maxKm+
+        '&minDate='+minDate+'&maxDate='+maxDate;
+
+    fetch(btnValid.dataset.link+attribut)
+        .then(reponse => reponse.json())
+        .then(data =>{
+
+           dataList = data.dataUsedCar;
+           divPaginator.innerHTML="";
+            supprimePaginator();
+            createPaginationButtons();
+           displayList();
+        })
+})
--- src/Entity/UsedCar.php
+++ src/Entity/UsedCar.php
@@ -190,10 +190,11 @@ class UsedCar
     }
     public function jsonSerialize(){
         return [
+            'id'=>$this->getId(),
             'prix' => $this->getPrix(),
             'anneeFabrication'=> $this->getAnneeFabrication()->format('Y'),
             'kilometrage'=> number_format($this->getKilometrage(), 0,'',' '),
-            'pathImage'=>'https://127.0.0.1:8001/image_voiture_occassion/'.$this->getLstImage()[0]->getPathImage()
+            'pathImage'=>'https://127.0.0.1:8000/image_voiture_occassion/'.$this->getLstImage()[0]->getPathImage()
         ];
     }
     public function getRandomImage()
