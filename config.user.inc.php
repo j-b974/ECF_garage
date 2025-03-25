b1c1de537f8b9a52295ab41607a13dbe85945aba
jb974 <bertil974@hotmail.fr>
2025-03-25 06:57:46 +0100

    dev gitAction

--- config.user.inc.php
+++ config.user.inc.php
@@ -1,5 +1,5 @@
 <?php
-$cfg['PmaAbsoluteUri'] = 'http://localhost/SQLmyAdmin/';
+$cfg['PmaAbsoluteUri'] = 'http://mysql.bertil.re';
 $cfg['ForceSSL'] = true; // Désactiver si tu n'utilises pas HTTPS
 $cfg['AllowArbitraryServer'] = true;
 $cfg['blowfish_secret'] = 'your-secret-key-here'; // Génère une clé de sécurité
